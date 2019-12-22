2a192cdc2bbbc113b1637643cff9e1dab512496d
Luke Gibbins <lukegibbins94@gmail.com>
2019-12-22 13:08:53 +0000

    Initial commit

---
+++ vuex-personal/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using vuex_personal.Models;
+
+namespace vuex_personal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetNames()
+        {
+            // returns view information
+            return Json("Vue Names will appear here...");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ vuex-personal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vuex_personal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
