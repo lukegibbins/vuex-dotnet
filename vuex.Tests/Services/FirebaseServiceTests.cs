1651742577e0d2f37456b84703e452f116e46336
Luke Gibbins <lukegibbins94@gmail.com>
2020-01-21 12:19:35 +0000

    Added testing projects

---
+++ vuex.Tests/Services/FirebaseServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using NUnit.Framework;
+
+namespace vuex.Tests.Services
+{
+    [TestFixture]
+    public class FirebaseServiceTests
+    {
+        IFirebaseClient client;
+
+        [SetUp]
+        public void Init()
+        {
+            IFirebaseConfig config = new FirebaseConfig()
+            {
+                AuthSecret = "RGEVKFH5XCflUf5CgIpLs6ljgc1v39Dt7SvZKmq4",
+                BasePath = "https://vuex-225ab.firebaseio.com"
+            };
+
+            client = new FirebaseClient(config);
+        }
+
+        [Test]
+        public void TestFirebaseConnection()
+        {
+            Assert.AreEqual((int) client.Get("/contacts").StatusCode, 200);
+        }
+    }
+}
