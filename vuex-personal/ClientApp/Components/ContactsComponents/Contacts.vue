9454a9213674193739aa0344309ab1a4c15c8000
Luke Gibbins <lukegibbins94@gmail.com>
2020-01-23 13:52:38 +0000

    Add watcher as a demonstration to watch for data changes for the contacts array

--- vuex-personal/ClientApp/Components/ContactsComponents/Contacts.vue
+++ vuex-personal/ClientApp/Components/ContactsComponents/Contacts.vue
@@ -102,6 +102,16 @@
       }
     },
 
+    // Used to demonstrate the functionality of a watcher.
+    // Deep is used to look inside of an array for its properties to see if any have changed
+    watch: {
+      contacts: {
+        deep: true,
+        handler() {
+          console.log("Watcher: contact data has changed");
+        },
+      }
+    },
 
     // Refers to constant data on the page. This data can never be modified
     // Props would be used if the projects array data listed below was required on multiple componenets, thus re-using code and not having static data on each page in the data object
